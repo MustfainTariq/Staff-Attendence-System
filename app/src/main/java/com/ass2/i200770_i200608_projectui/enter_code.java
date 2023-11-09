cf44efff4cd94248512ca8547150801ba7a0e4d2
huzaifasaleemawan <i200608@nu.edu.pk>
2023-11-09 23:27:50 +0500

    By Huzaifa Saleem Awan

---
+++ app/src/main/java/com/ass2/i200770_i200608_projectui/enter_code.java
@@ -0,0 +1,14 @@
+package com.ass2.i200770_i200608_projectui;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class enter_code extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_code);
+    }
+}
